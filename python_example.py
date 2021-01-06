00a7f1f8b5cd8ac9ca72a51e3e98656a01f0d5b8
LAPTOP-IB4DPRIG\rantz <ran.tzur@testproject.io>
2021-01-06 12:47:41 +0200

    fixup! fixup! fixup! fixup! Updated config

--- python_example.py
+++ python_example.py
@@ -1,14 +1,15 @@
 from src.testproject.sdk.drivers import webdriver
 from selenium.webdriver import ChromeOptions
+import pytest
+import os
 
-
-def simple_test():
+def test_simple_test():
     driver = webdriver.Chrome()
 
     driver.get("https://example.testproject.io/web/")
 
     driver.find_element_by_css_selector("#name").send_keys("John Smith")
-    driver.find_element_by_css_selector("#password").send_keys("12345")
+    driver.find_element_by_css_selector("#password").send_keys(os.environ['PASSWORD'])
     driver.find_element_by_css_selector("#login").click()
 
     passed = driver.find_element_by_css_selector("#logout").is_displayed()
@@ -16,7 +17,3 @@ def simple_test():
     print("Test passed") if passed else print("Test failed")
 
     driver.quit()
-
-
-if __name__ == "__main__":
-    simple_test()
